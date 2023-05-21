dd7696e9025f16959c6a8dff457b13e9c4319610
Kurbanova Zhavgarat <jkurbanova85@gmail.com>
2023-05-22 02:39:10 +0300

    feat: реализовать тесты для hf,jns c json для dto (всех сущностей), реализовать тесты для слоя репозиториев, которые содержат кастомные запросы(temepositoryest, ookingepositoryest)

--- src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -1,24 +1,129 @@
 package ru.practicum.shareit.booking;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.ItemRepository;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.UserRepository;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class BookingRepositoryTest {
 
+    @Autowired
+    private BookingRepository repository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+    private PageRequest page;
+
+    @BeforeEach
+    void before() {
+        User user = new User();
+        user.setName("user");
+        user.setEmail("user@user.com");
+
+        Item item = new Item();
+        item.setName("Щётка для обуви");
+        item.setDescription("Стандартная щётка для обуви");
+        item.setAvailable(true);
+        item.setOwner(user);
+
+        Item item2 = new Item();
+        item2.setName("Краска для обуви");
+        item2.setDescription("Стандартная краска для обуви");
+        item2.setAvailable(true);
+        item2.setOwner(user);
+
+        User userBooker = new User();
+        userBooker.setName("userBooker");
+        userBooker.setEmail("userBooker@user.com");
+
+        userRepository.save(user);
+        itemRepository.save(item);
+        itemRepository.save(item2);
+        userRepository.save(userBooker);
+
+        Booking booking = new Booking();
+        booking.setStart(LocalDateTime.now());
+        booking.setEnd(LocalDateTime.now().plusDays(2));
+        booking.setStatus(BookingStatus.WAITING);
+        booking.setItem(item2);
+        booking.setBooker(userBooker);
+
+        repository.save(booking);
+
+        int from = 0;
+        int size = 10;
+        page = PageRequest.of(from > 0 ? from / size : 0, size);
+    }
     @Test
     void findByIdAndBookerIdOrItemOwnerId() {
+        Optional<Booking> booking = repository.findByIdAndBookerIdOrItemOwnerId(1L, 2L);
+
+        assertTrue(booking.isPresent());
+    }
+
+    @Test
+    void findByIdAndInvalidBookerIdOrItemOwnerIdThenReturnedEmptyList() {
+        Optional<Booking> booking = repository.findByIdAndBookerIdOrItemOwnerId(1L, 3L);
+
+        assertTrue(booking.isEmpty());
     }
 
     @Test
     void findByBookerIdAndCurrentMomentBetweenStartAndEnd() {
+        List<Booking> bookingList = repository.findByBookerIdAndCurrentMomentBetweenStartAndEnd(2L,
+                LocalDateTime.now(), page).getContent();
+
+        assertEquals(1, bookingList.size(), "Размер списка не равен 1");
+        assertEquals(1L, bookingList.get(0).getId(), "Значения не равны");
+    }
+
+    @Test
+    void findByInvalidBookerIdAndCurrentMomentBetweenStartAndEndThenReturnedEmptyList() {
+        List<Booking> bookingList = repository.findByBookerIdAndCurrentMomentBetweenStartAndEnd(1L,
+                LocalDateTime.now(), page).getContent();
+
+        assertTrue(bookingList.isEmpty());
     }
 
     @Test
     void findByItemOwnerIdAndCurrentMomentBetweenStartAndEnd() {
+        List<Booking> bookingList = repository.findByItemOwnerIdAndCurrentMomentBetweenStartAndEnd(1L,
+                LocalDateTime.now(), page).getContent();
+
+        assertEquals(1, bookingList.size(), "Размер списка не равен 1");
+        assertEquals(1L, bookingList.get(0).getId(), "Значения не равны");
     }
 
     @Test
-    void getBookingDate() {
+    void findByItemOwnerIdAndCurrentMomentBetweenStartAndEndThenReturnedEmptyList() {
+        List<Booking> bookingList = repository.findByItemOwnerIdAndCurrentMomentBetweenStartAndEnd(2L,
+                LocalDateTime.now(), page).getContent();
+
+        assertTrue(bookingList.isEmpty());
+    }
+
+    @Test
+    void getBookingDateThenReturnedList() {
+        List<Booking> bookingList = repository.getBookingDate(2L,
+                LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(3));
+
+        assertEquals(1, bookingList.size(), "Размер списка не равен 1");
+        assertEquals(1L, bookingList.get(0).getId(), "Значения не равны");
     }
 }
--- src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
@@ -1,12 +1,77 @@
 package ru.practicum.shareit.item;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.UserRepository;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class ItemRepositoryTest {
 
+    @Autowired
+    private ItemRepository repository;
+
+    @Autowired
+    private UserRepository userRepository;
+    private PageRequest page;
+
+    @BeforeEach
+    void setUp() {
+        User user = new User();
+        user.setName("user");
+        user.setEmail("user@user.com");
+
+        Item item = new Item();
+        item.setName("Щётка для обуви");
+        item.setDescription("Стандартная щётка для обуви");
+        item.setAvailable(true);
+        item.setOwner(user);
+
+        Item item2 = new Item();
+        item2.setName("Краска для обуви");
+        item2.setDescription("Стандартная краска для обуви");
+        item2.setAvailable(true);
+        item2.setOwner(user);
+
+        userRepository.save(user);
+        repository.save(item);
+        repository.save(item2);
+
+        int from = 0;
+        int size = 10;
+        page = PageRequest.of(from > 0 ? from / size : 0, size);
+    }
+
     @Test
-    void search() {
+    void searchByTextThenReturnedItem() {
+        String text = "краска";
+
+        List<Item> items = repository.search(text, page).toList();
+
+        assertEquals(1, items.size(), "Размер списка не равен 1");
+        assertEquals("Краска для обуви", items.get(0).getName(), "Значения не равны");
+    }
+
+    @Test
+    void searchByTextThenReturnedEmptyList() {
+        String text = "дрель";
+
+        List<Item> items = repository.search(text, page).toList();
+
+        assertTrue(items.isEmpty());
+    }
+    @AfterEach
+    void deleteAll() {
+        repository.deleteAll();
+        userRepository.deleteAll();
     }
 }
